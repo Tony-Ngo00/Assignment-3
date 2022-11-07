1a862e0d3adc7570c0401a095645b760036c64d3
NgoTony <ngotony00@gmail.com>
2022-11-06 19:10:33 -0500

    Made the selected element displayed using 'Get' button

--- script.js
+++ script.js
@@ -1,30 +1,43 @@
-let response = axios.get("https://api.themoviedb.org/3/search/movie", {
-  params: {
-    api_key: "779ebe30f392f779f18a739e5df2f414",
-    include_adult: "false",
+function getOption() {
+  for (let x = 0; x < 10; x++) {
+    if (document.getElementById("Movies").selectedIndex == x) {
+      return document.getElementById("Movies").value
+    }
   }
-});
+};
 
-response = response.then((moviesData) => {
-  for (let movie of moviesData.data.results) {   
-    axios.get(`https://api.themoviedb.org/3/movie/${movie.id}`, {
-      params: {
-        api_key: "779ebe30f392f779f18a739e5df2f414",
-        append_to_response: "videos",
-      }
-    }).then((movieData) => {
-      const img = document.createElement('img');
-      const p = document.createElement('p');
-      const iframe = document.createElement('iframe');
+console.log(getOption())
 
-      const trailers = movieData.data.videos.results.filter((trailer) => trailer.type === "Trailer");
-      iframe.src = `https://www.youtube.com/embed/${trailers.at(0).key}`
-      img.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
-      p.innerHTML = `${movie.title} -- ${movie.release_date} -- ${movie.popularity}`;
-      
-      document.body.append(p);
-      document.body.append(img);
-      document.body.append(iframe);
-    });
-  }
+button.addEventListener('click', async () => {
+  let response = axios.get("https://api.themoviedb.org/3/search/movie", {
+    params: {
+      api_key: "779ebe30f392f779f18a739e5df2f414",
+      include_adult: "false",
+      query: getOption(),
+    }
+  });
+
+  response = response.then((moviesData) => {
+    for (let movie of moviesData.data.results) {
+      axios.get(`https://api.themoviedb.org/3/movie/${movie.id}`, {
+        params: {
+          api_key: "779ebe30f392f779f18a739e5df2f414",
+          append_to_response: "videos",
+        }
+      }).then((movieData) => {
+        const img = document.createElement('img');
+        const p = document.createElement('p');
+        const iframe = document.createElement('iframe');
+
+        const trailers = movieData.data.videos.results.filter((trailer) => trailer.type === "Trailer");
+        iframe.src = `https://www.youtube.com/embed/${trailers.at(0).key}`
+        img.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+        p.innerHTML = `${movie.title} <br> Release Date: ${movie.release_date}`;
+
+        document.body.append(p);
+        document.body.append(img);
+        document.body.append(iframe);
+      });
+    }
+  });
 });
