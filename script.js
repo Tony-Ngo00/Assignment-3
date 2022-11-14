74a4aebe9d8a015ada8fd6ae314499408e7fb123
Tony-Ngo00 <ngotony00@gmail.com>
2022-11-14 14:46:27 -0500

    Fix

--- script.js
+++ script.js
@@ -20,9 +20,6 @@ button.addEventListener('click', async () => {
     const cover = document.getElementById('cover');
     const video = document.getElementById('video');
 
-    const display = document.getElementById('display');
-    display.removeAttribute('hidden')
-
     const trailers = selectedMovie.videos.results.filter((trailer) => trailer.type === "Trailer");
     video.src = `https://www.youtube.com/embed/${trailers.at(0).key}`
     cover.src = `https://image.tmdb.org/t/p/w500${selectedMovie.poster_path}`;
@@ -40,6 +37,9 @@ button.addEventListener('click', async () => {
     info.innerHTML = `Title: ${selectedMovie.title} <br> Original Title: ${selectedMovie.original_title} <br> Genre: ${genres} <br> Release Date: ${selectedMovie.release_date} <br> Popularity: ${selectedMovie.popularity} <br> Revenue: ${formatter.format(selectedMovie.revenue)} <br> Vote Average: ${selectedMovie.vote_average} <br> Vote Count: ${selectedMovie.vote_count} <br> Runtime: ${selectedMovie.runtime} mins`;
     overviewLabel.innerHTML = "Overview:";
     overviewText.innerHTML = `${selectedMovie.overview}`;
+
+    const display = document.getElementById('display');
+    display.removeAttribute('hidden')
   });
 });
 
